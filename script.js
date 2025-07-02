80e328de70df8487f5ce6b48a3e3953fdf1ee2a9
Naman Jha <itsnaman0007@gmail.com>
2025-07-02 12:22:15 +0530

    script.js

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ if (form) {
   form.addEventListener("submit", function (e) {
     e.preventDefault();
     const data = new FormData(form);
-    const sheetURL = "https://script.google.com/macros/s/AKfycbxjAJlg1ruVFHAvj0KT7x4rjQNDoHyEl2QtnkAjXr4TgorL22sfqJbnyAJhNpDgrNIO/exec"; // Replace with actual Apps Script URL
+    const sheetURL = "https://script.google.com/macros/s/AKfycby4jb7qKTk40iRHxnOeOIMxe9-VHjBWiaFcNKIhujZ1_9tKj479urKRd1WpOS-ccIFG/exec"; // Replace with actual Apps Script URL
 
     fetch(sheetURL, {
       method: "POST",
